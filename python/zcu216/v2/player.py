44ab2c899ad4cb6a43b24f2315b5b893a32f01c2
Eugene Feinberg <eugene@undertow.ai>
2023-02-19 15:54:01 -0800

    V2 dac player mmap driver

--- python/zcu216/v2/player.py
+++ python/zcu216/v2/player.py
@@ -10,17 +10,23 @@ class player():
 
         size = (size // mmap.PAGESIZE) * mmap.PAGESIZE
         self.size = size >> 1 # size of int16
-        print(self.size)
+        print(self.size, size)
 
-        f = os.open("/dev/mem", os.O_RDWR)
-        m = mmap.mmap(f, size, flags=mmap.MAP_SHARED, prot=mmap.PROT_WRITE|mmap.PROT_READ, offset=base)
-        
+        f = os.open("/dev/mem", os.O_RDWR, os.O_SYNC)
+
+        assert (f != None)
+
+        self.m = mmap.mmap(f, size, flags=mmap.MAP_SHARED, prot=mmap.PROT_WRITE|mmap.PROT_READ, offset=base)
+
         # Create 16 channel buffers 
-        self.channels = np.frombuffer(m, dtype=np.int16, count=self.size, offset=0).reshape(16,-1)
-
+        self.channels = np.frombuffer(self.m, dtype=np.int16, count=self.size, offset=0).reshape(16,-1)
+        print(self.channels.shape)
 
 if __name__ == "__main__":
 
     p = player(int('b2000000',16), 512*1024)
+    p.channels[0] = ((np.sin(np.linspace(start=0,stop=2*np.pi,num=16384)) * 100.0)).astype(np.int16)
+    p.channels[1] = ((np.cos(np.linspace(start=0,stop=2*np.pi,num=16384)) * 100.0)).astype(np.int16)
 
-    print(p.channels[0:128])
+    print(p.channels[0][0:16384:100])
+    print(p.channels[1][0:16384:100])
