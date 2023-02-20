ff164dd6d31de6bcbf27fcdc9c798d928ae704a2
Eugene Feinberg <eugene@undertow.ai>
2023-02-19 22:12:39 -0800

    Working basic v2 test

--- python/zcu216/test/test_v2.py
+++ python/zcu216/test/test_v2.py
@@ -1,9 +1,16 @@
 import os
+import numpy as np
+import time
+import zmq
+import json
+from json import JSONEncoder
 
 from zcu216.rfdc.rfdc_clk import RfdcClk
 from zcu216.axi.gpio import AxiGpio
 
-    
+from zcu216.v2.player import player as V2Player
+from zcu216.v2.capture import capture as V2Capture
+from zcu216.v2.sequencer import sequencer as V2Sequencer    
 
 class TestV2():
 
@@ -16,12 +23,38 @@ class TestV2():
         print("Test init done\n")
         return None
 
-    
 if __name__ == "__main__":
+
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.setsockopt(zmq.SNDHWM, 2)
+    socket.bind("tcp://*:5555")
 
     test = TestV2();
 
+    # Ugly mess - replace with symbol lookup in /proc
+    p = V2Player(int('b2000000',16), 512*1024)
+    c = V2Capture(int('b1000000', 16), 1024*1024)
+    s = V2Sequencer(int("b0100000",16))
 
+    # load a sin wave in each channel
+    for ch in range(0,16):
+        p.channels[ch] = ((np.sin(np.linspace(start=0,stop=5*2*np.pi,num=16384))) * 32767).astype(np.int16)
+
+    # Run a capture
+    for i in range(0,1):
+        s.StartSequencer(captures=1,symbol_samples=16384,integrations=4000,dwells=1,offset=0)
+       
+        # Wait for sequencer to finish
+        while (s.SequencerRunning()):
+            pass
+
+        # Copy out capture data
+        channel_data = c.channels
+
+        np.save("channel_data.npy", channel_data)
+
+        print("Iteration %d" % i)
 
     exit(0)
 
--- python/zcu216/v2/sequencer.py
+++ python/zcu216/v2/sequencer.py
@@ -4,6 +4,18 @@ import numpy as np
 
 class sequencer():
 
+    CAPTURE_COUNT='capture count'
+    CURRENT_CAPTURE='current capture'
+    SYMBOL_CYCLES='symbol cycles'
+    CURRENT_SYMBOL='current symbol'
+    SYMBOL_INTEGRATION='symbol integrations'
+    CURRENT_INTEGRATION='current integration'
+    DWELL_TIMES='dwell times'
+    CURRENT_DWELL_TIME='current dwell time'
+    OFFSET_TIME='offset time'
+    CURRENT_OFFSET_TIME='current offset time'
+    LOOPBACK_ENABLE='loopback enable'
+
     reg_defs = { 'capture count': 0,
                  'current capture': 1,
                  'symbol cycles': 4,
@@ -21,14 +33,37 @@ class sequencer():
         f = os.open("/dev/mem", os.O_RDWR)
         assert (f != None)
 
-        print("Mapping at %08x\n", base)
+        print("Mapping at %08x\n" % base)
 
         self.m = mmap.mmap(f,mmap.PAGESIZE,mmap.MAP_SHARED,mmap.PROT_WRITE|mmap.PROT_READ, offset=base)
         assert (self.m != None)
 
         self.regs = np.frombuffer(self.m, np.uint32, mmap.PAGESIZE >> 4)
-        print(self.regs[0])
 
+    def StartSequencer(self,
+                       captures: int,
+                       symbol_samples: int,
+                       integrations: int,
+                       dwells: int,
+                       offset: int
+                       ) -> None:
+        
+        self.regs[self.reg_defs['current capture']] = 0
+        self.regs[self.reg_defs['current symbol']] = 0
+        self.regs[self.reg_defs['current integration']] = 0
+        self.regs[self.reg_defs['current dwell time']] = 0
+        self.regs[self.reg_defs['current offset time']] = 0
+
+        self.regs[self.reg_defs['symbol cycles']] = symbol_samples
+        self.regs[self.reg_defs['symbol integrations']] = integrations
+        self.regs[self.reg_defs['dwell times']] = dwells
+        self.regs[self.reg_defs['offset time']] = offset
+
+        # and finally start 
+        self.regs[self.reg_defs['capture count']] = captures
+
+    def SequencerRunning(self) -> bool:
+        return self.regs[self.reg_defs['capture count']] != self.regs[self.reg_defs['current capture']]
 
     def SetReg(self, offset: int, val: int) -> None:
         self.regs[offset] = val
