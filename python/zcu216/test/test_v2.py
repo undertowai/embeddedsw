15b582cdf40c050bd5ce475b141e04f4b6aad89e
Eugene Feinberg <eugene@undertow.ai>
2023-02-21 19:59:57 -0800

    ZMQ example of v2 test

--- python/zcu216/test/test_v2.py
+++ python/zcu216/test/test_v2.py
@@ -18,6 +18,7 @@ class TestV2():
         self.AxiGpio = AxiGpio("axi_gpio");
         self.RfdcClk = RfdcClk()
 
+        self.RfdcClk.init_clk104(lmk_path="zcu216/rfdc/configs/LMK_300MHz_RefClk_Out.txt")
         self.RfdcClk.setup_rfdc()
 
         print("Test init done\n")
@@ -37,12 +38,14 @@ if __name__ == "__main__":
     c = V2Capture(int('b1000000', 16), 1024*1024)
     s = V2Sequencer(int("b0100000",16))
 
-    # load a sin wave in each channel
+    # load a sin wave in each channel with a different phase offset
     for ch in range(0,16):
-        p.channels[ch] = ((np.sin(np.linspace(start=0,stop=5*2*np.pi,num=16384))) * 32767).astype(np.int16)
+        start = 0 + np.pi * (ch / 16.0)
+        stop  = 5 * (2 * np.pi) + start
+        p.channels[ch] = ((np.sin(np.linspace(start=start,stop=stop,num=16384))) * 32767).astype(np.int16)
 
     # Run a capture
-    for i in range(0,1):
+    for i in range(0,100):
         s.StartSequencer(captures=1,symbol_samples=16384,integrations=4000,dwells=1,offset=0)
        
         # Wait for sequencer to finish
@@ -52,7 +55,12 @@ if __name__ == "__main__":
         # Copy out capture data
         channel_data = c.channels
 
-        np.save("channel_data.npy", channel_data)
+        # send binary data 16 channels of int32 with implicit shape (16,16384)
+        # receive with 
+        #      m = socket.recv()
+        #      channels = np.frombuffer(m, dtype=np.int32).reshape((16,-1))
+        
+        socket.send(channel_data)
 
         print("Iteration %d" % i)
 
